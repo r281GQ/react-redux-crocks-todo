c4fb1b9884fed6e66dc92e92eba48c894365a795
Endre Vegh <endre.vegh@yahoo.com>
2019-01-04 09:00:26 +0100

    refactor reducer

--- src/data/reducers/todo.js
+++ src/data/reducers/todo.js
@@ -1,21 +1,15 @@
+import State from "crocks/State";
 import { TOGGLE } from "./../constants";
 
-const handleToggle = (prevState, action) => {
-  const idx = prevState.todos.findIndex(item => item.id === action.payload.id);
-
-  const cp = [...prevState.todos];
-
-  cp[idx].completed = !cp[idx].completed;
-
-  return { ...prevState, todos: cp };
-};
-
-export const todos = (prevState, action) => {
+/**
+ *  todos :: Action -> State
+ */
+export const todos = action => {
   switch (action.type) {
     case TOGGLE:
-      return handleToggle(prevState, action);
+      return State.of(null);
 
     default:
-      return prevState;
+      return State.of(null);
   }
 };
